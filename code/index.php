5c90566535209e58e71231ccf4058ff502f48692
NikolaySHCHeglov <n_sheglov@mail.ru>
2022-02-22 14:54:02 +0200

    2-nd updating index.php

--- code/index.php
+++ code/index.php
@@ -8,3 +8,13 @@ $order .=" and 15 strips and 40% discount";
 
 echo"\nYour order is:$very_bad_unclear_name.";
 
+$number = 23;
+echo"<br><br>$number";
+echo"<br>";
+$FloatNumber = 3.1415926;
+echo"$FloatNumber"."<br>";
+$negative = -11;
+echo($number + $negative)."<br>";
+$last_month = 1187.23;
+$this_month = 1089.98;
+echo($last_month - $this_month)."<br>";
