ae2aaefafaf9ed481a4e39204f08dce7093342fb
NikolaySHCHeglov <n_sheglov@mail.ru>
2022-02-22 14:32:14 +0200

    1st updating index.php

--- code/index.php
+++ code/index.php
@@ -1,2 +1,10 @@
 <?php
-echo phpinfo();
+/* Imagine a lot of code here */
+$very_bad_unclear_name = "15 chicken wings";
+
+// Write your code here:
+$order = &$very_bad_unclear_name;
+$order .=" and 15 strips and 40% discount";
+
+echo"\nYour order is:$very_bad_unclear_name.";
+
